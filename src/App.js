d45609236145527f7ec27ff9cd5538b8af4e4cdb
Matias Verdejo <matias.verdejo@houm.com>
2023-06-27 10:40:47 -0400

    feat(sesion-2): router, context, hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useEffect, useState } from "react";
+import "./App.css";
+import { useContext } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+
+const authenticationContext = createContext({
+  isLoggedIn: false,
+  handleLogin: () => {},
+  handleLogout: () => {},
+});
+
+export const AuthenticationProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    Boolean(localStorage.getItem("isLoggedIn"))
+  );
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/beers");
+    localStorage.setItem("isLoggedIn", "true");
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    navigate("/");
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    if(!isLoggedIn){
+      navigate("/");
+    }
+  },[isLoggedIn, navigate]);
+
+  const redirectURL = isLoggedIn ? "/beers" : "/";
+
+  return (
+    <authenticationContext.Provider
+      value={{
+        isLoggedIn,
+        handleLogin,
+        handleLogout,
+        redirectURL,
+      }}
+    >
+      {children}
+    </authenticationContext.Provider>
+  );
+};
+
+const useAuthentication = () => {
+  const authContext = useContext(authenticationContext);
+  if (!authContext) {
+    throw new Error(
+      "useAuthentication debe estar dentro del proveedor de contexto authenticationContext"
+    );
+  }
+  return authContext;
+};
+
+const BeersApp = () => {
+  const { handleLogout } = useAuthentication();
+  return (
+    <div>
+      <h2>Beers App</h2>
+      <button type="button" onClick={handleLogout}>
+        logout
+      </button>
+    </div>
+  );
+};
+
+const Landing = () => {
+  const { handleLogin } = useAuthentication();
+  return (
+    <div>
+      <h2>Landing</h2>
+      <button type="button" onClick={handleLogin}>
+        login
+      </button>
+    </div>
+  );
+};
 
 function App() {
+  const { isLoggedIn, redirectURL } = useAuthentication();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Nuestra super app</h1>
+        <h4>{isLoggedIn ? "bienvenido" : "debes iniciar sesión"}</h4>
+        <Routes>
+          <Route path="/beers" Component={BeersApp} />
+          <Route path="/" Component={Landing} />
+          <Route path="*" element={<Navigate to={redirectURL} replace />}/>
+        </Routes>
+      </div>
   );
 }
 
